313d993fae40c1fe3daf260d6534ccb1326eb236
Mansab Uppal <muppal@thoughtworks.com>
2023-05-07 01:58:13 +0200

    refactor, split methods, add app name

--- cctray.py
+++ cctray.py
@@ -1,49 +1,38 @@
 import os
-import requests
+import requests as req
 import xml.etree.ElementTree as ET
 from flask import Flask, request, make_response
 
-app = Flask(__name__)
 
+app = Flask('github-cctray')
 
-@app.route('/', methods=['GET', 'POST'])
+def get_workflow_runs(owner, repo, token):
+    endpoint = f"https://api.github.com/repos/{owner}/{repo}/actions/runs"
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    response = req.get(endpoint, headers=headers)
+    return response.json()
+
+@app.route('/')
 def index():
     owner = request.args.get("owner") or request.form.get('owner')
     repo = request.args.get("repo") or request.form.get('repo')
     token = os.environ.get("GITHUB_TOKEN")
 
-    # GitHub API endpoint to retrieve workflow runs for a repository
-    endpoint = "https://api.github.com/repos/{owner}/{repo}/actions/runs"
-
-    # Check if the required parameters are present and not empty
     if not owner or not repo or not token:
         return make_response("Missing parameters", 400)
 
-    # Build the API URL
-    url = endpoint.format(owner=owner, repo=repo)
+    data = get_workflow_runs(owner, repo, token)
 
-    # Add authentication to the request headers
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Accept": "application/vnd.github.v3+json"
-    }
-
-    # Send the API request
-    response = requests.get(url, headers=headers)
-
-    data = response.json()
-
-    # Create root element for CCTray XML response
     root = ET.Element("Projects")
 
-    # Loop through each workflow run and create a project element for each
     for run in data["workflow_runs"]:
         project = ET.SubElement(root, "Project")
 
-        # Set project name
         project.set("name", repo)
 
-        # Set last build status based on conclusion
         if run["conclusion"] == "success":
             project.set("lastBuildStatus", "Success")
         elif run["conclusion"] == "failure":
@@ -51,17 +40,13 @@ def index():
         else:
             project.set("lastBuildStatus", "Unknown")
 
-        # Set last build time
         project.set("lastBuildTime", run["updated_at"])
 
-        # Set web URL for project
         project.set("webUrl", run["html_url"])
 
-    # Return CCTray XML response
     response = make_response(ET.tostring(root).decode())
     response.headers['Content-Type'] = 'application/xml'
     return response
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
