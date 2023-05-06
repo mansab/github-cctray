34cedbdf1a1c994f5af830d0c253ae52c5c026ea
Mansab Uppal <muppal@thoughtworks.com>
2023-05-07 00:31:02 +0200

    Update the sctipt to use Github actions/runs API

--- cctray.py
+++ cctray.py
@@ -2,77 +2,54 @@ import os
 import requests
 import xml.etree.ElementTree as ET
 
-# Get the GitHub repository information from environment variables
-username = os.environ.get("GITHUB_USERNAME")
-repository = os.environ.get("GITHUB_REPOSITORY")
+owner = os.environ.get("GITHUB_OWNER")
+repo = os.environ.get("GITHUB_REPOSITORY")
 token = os.environ.get("GITHUB_TOKEN")
 
+# GitHub API endpoint to retrieve workflow runs for a repository
+endpoint = "https://api.github.com/repos/{owner}/{repo}/actions/runs"
+
 # Check if the environment variables are set and not empty
-if not username or not repository or not token:
+if not owner or not repo or not token:
     raise ValueError("Required environment variables are missing or empty")
 
-# Set the URL of the GitHub Workflow API for the repository
-url = f"https://api.github.com/repos/{username}/{repository}/actions/workflows"
+# Build the API URL
+url = endpoint.format(owner=owner, repo=repo)
 
-# Set the authorization headers for the API request
-headers = {"Authorization": f"token {token}"}
+# Add authentication to the request headers
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Accept": "application/vnd.github.v3+json"
+}
 
-# Make a request to the API to get information about the workflows
+# Send the API request
 response = requests.get(url, headers=headers)
 
-# Parse the response as JSON
 data = response.json()
 
-# Create the root element of the XML tree
+# Create root element for CCTray XML response
 root = ET.Element("Projects")
 
-# Loop through each workflow in the response
-for workflow in data["workflows"]:
-    # Create a new project element for this workflow
+# Loop through each workflow run and create a project element for each
+for run in data["workflow_runs"]:
     project = ET.SubElement(root, "Project")
 
-    # Set the name of the project to the name of the workflow
-    project.set("name", workflow["name"])
+    # Set project name
+    project.set("name", repo)
 
-    # Set the activity of the project based on the status of the latest run
-    if "runs" in workflow and len(workflow["runs"]) > 0:
-        latest_run = workflow["runs"][0]
-        if latest_run["status"] == "completed":
-            if latest_run["conclusion"] == "success":
-                project.set("activity", "Sleeping")
-            else:
-                project.set("activity", "Building")
-        else:
-            project.set("activity", "Building")
-    else:
-        project.set("activity", "Sleeping")
-
-    # Set the last build status of the project based on the status of the latest run
-    if "runs" in workflow and len(workflow["runs"]) > 0:
-        latest_run = workflow["runs"][0]
-        if latest_run["status"] == "completed":
-            if latest_run["conclusion"] == "success":
-                project.set("lastBuildStatus", "Success")
-            else:
-                project.set("lastBuildStatus", "Failure")
-        else:
-            project.set("lastBuildStatus", "Unknown")
+    # Set last build status based on conclusion
+    if run["conclusion"] == "success":
+        project.set("lastBuildStatus", "Success")
+    elif run["conclusion"] == "failure":
+        project.set("lastBuildStatus", "Failure")
     else:
         project.set("lastBuildStatus", "Unknown")
 
-    # Set the URL of the project to the URL of the workflow on GitHub
-    project.set("webUrl", workflow["html_url"])
+    # Set last build time
+    project.set("lastBuildTime", run["updated_at"])
 
-    # Set the last build time of the project to the timestamp of the latest run
-    if "runs" in workflow and len(workflow["runs"]) > 0:
-        latest_run = workflow["runs"][0]
-        project.set("lastBuildTime", latest_run["updated_at"])
-    else:
-        project.set("lastBuildTime", "")
+    # Set web URL for project
+    project.set("webUrl", run["html_url"])
 
-# Generate the XML string from the XML tree
-xml_string = ET.tostring(root, encoding="unicode")
-
-# Return the XML string
-print(xml_string)
-
+# Return CCTray XML response
+print(ET.tostring(root).decode())
