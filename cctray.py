286aa2fd87099dd19c5a3d902098141446b78b2f
Mansab Uppal <muppal@thoughtworks.com>
2023-05-07 00:44:52 +0200

    Run it as a webapp and pass params

--- cctray.py
+++ cctray.py
@@ -1,55 +1,67 @@
 import os
 import requests
 import xml.etree.ElementTree as ET
+from flask import Flask, request, make_response
 
-owner = os.environ.get("GITHUB_OWNER")
-repo = os.environ.get("GITHUB_REPOSITORY")
-token = os.environ.get("GITHUB_TOKEN")
+app = Flask(__name__)
 
-# GitHub API endpoint to retrieve workflow runs for a repository
-endpoint = "https://api.github.com/repos/{owner}/{repo}/actions/runs"
 
-# Check if the environment variables are set and not empty
-if not owner or not repo or not token:
-    raise ValueError("Required environment variables are missing or empty")
+@app.route('/')
+def index():
+    owner = request.args.get("owner")
+    repo = request.args.get("repo")
+    token = os.environ.get("GITHUB_TOKEN")
 
-# Build the API URL
-url = endpoint.format(owner=owner, repo=repo)
+    # GitHub API endpoint to retrieve workflow runs for a repository
+    endpoint = "https://api.github.com/repos/{owner}/{repo}/actions/runs"
 
-# Add authentication to the request headers
-headers = {
-    "Authorization": f"Bearer {token}",
-    "Accept": "application/vnd.github.v3+json"
-}
+    # Check if the required parameters are present and not empty
+    if not owner or not repo or not token:
+        return make_response("Missing parameters", 400)
 
-# Send the API request
-response = requests.get(url, headers=headers)
+    # Build the API URL
+    url = endpoint.format(owner=owner, repo=repo)
 
-data = response.json()
+    # Add authentication to the request headers
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
 
-# Create root element for CCTray XML response
-root = ET.Element("Projects")
+    # Send the API request
+    response = requests.get(url, headers=headers)
 
-# Loop through each workflow run and create a project element for each
-for run in data["workflow_runs"]:
-    project = ET.SubElement(root, "Project")
+    data = response.json()
 
-    # Set project name
-    project.set("name", repo)
+    # Create root element for CCTray XML response
+    root = ET.Element("Projects")
 
-    # Set last build status based on conclusion
-    if run["conclusion"] == "success":
-        project.set("lastBuildStatus", "Success")
-    elif run["conclusion"] == "failure":
-        project.set("lastBuildStatus", "Failure")
-    else:
-        project.set("lastBuildStatus", "Unknown")
+    # Loop through each workflow run and create a project element for each
+    for run in data["workflow_runs"]:
+        project = ET.SubElement(root, "Project")
 
-    # Set last build time
-    project.set("lastBuildTime", run["updated_at"])
+        # Set project name
+        project.set("name", repo)
 
-    # Set web URL for project
-    project.set("webUrl", run["html_url"])
+        # Set last build status based on conclusion
+        if run["conclusion"] == "success":
+            project.set("lastBuildStatus", "Success")
+        elif run["conclusion"] == "failure":
+            project.set("lastBuildStatus", "Failure")
+        else:
+            project.set("lastBuildStatus", "Unknown")
 
-# Return CCTray XML response
-print(ET.tostring(root).decode())
+        # Set last build time
+        project.set("lastBuildTime", run["updated_at"])
+
+        # Set web URL for project
+        project.set("webUrl", run["html_url"])
+
+    # Return CCTray XML response
+    response = make_response(ET.tostring(root).decode())
+    response.headers['Content-Type'] = 'application/xml'
+    return response
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
